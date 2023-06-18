ce9615329375d70891aa8fca4c469b3e40bfe36c
Anton Kulagin <i-gorusich@mail.ru>
2023-06-18 14:38:57 +0300

    created functionality

---
+++ src/components/FilterButton.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { completeCount, getIsFiltered, setIsFiltered } from 'src/store/taskSlice'
+
+export const FilterButton = () => {
+  const dispatch = useDispatch()
+  const isFiltered = useSelector(getIsFiltered)
+  const completeCountItems = useSelector(completeCount)
+
+  const [showCompleteTasks, setShowCompleteTasks] = useState(isFiltered)
+
+  useEffect(() => {
+    dispatch(setIsFiltered(showCompleteTasks))
+  }, [showCompleteTasks])
+
+  const handleClick = () => {
+    setShowCompleteTasks(prev => !prev)
+  }
+  return (
+    <div className="filter">
+      <button className="filter-button" onClick={handleClick}>
+        {!showCompleteTasks ? 'Скрыть выполненные задачи' : 'Показать выполненные задачи:'}
+      </button>
+      {showCompleteTasks && <div className="filter-complete-count">{completeCountItems} шт.</div>}
+    </div>
+  )
+}
