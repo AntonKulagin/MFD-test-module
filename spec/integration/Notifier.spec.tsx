345608a45bf24de2190d60ebc064383ca8507e4f
Anton Kulagin <i-gorusich@mail.ru>
2023-06-25 13:57:23 +0300

    test for notifier

--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,30 @@
-import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import { render, screen } from '@testing-library/react'
+import { Notifier } from 'src/components/Notifier'
 
 describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
-});
+  it.each([[true], [false]])('появляется и содержит заголовок задачи', async open => {
+    const fn = jest.fn()
+    render(<Notifier open={open} task="первая задача" onClose={fn} />)
+
+    jest.runAllTimers()
+
+    const taskText = screen.queryByText(/первая задача/i)
+
+    if (taskText) {
+      expect(taskText).toBeInTheDocument()
+    } else {
+      expect(taskText).not.toBeInTheDocument()
+    }
+  })
+  it.each([
+    [true, 1],
+    [false, 0],
+  ])('одновременно может отображаться только одно', (open, showTimes) => {
+    const fn = jest.fn()
+    render(<Notifier open={open} task="первая задача" onClose={fn} />)
+
+    jest.runAllTimers()
+
+    expect(fn).toBeCalledTimes(showTimes)
+  })
+})
